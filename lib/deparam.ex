d4cc0b5d6c0c6b4dbe4c2bcc37c75c8075a28ce2
Tobias Casper <tobias.casper@gmail.com>
2021-03-08 17:51:37 +0100

    init project

---
+++ lib/deparam.ex
@@ -0,0 +1,18 @@
+defmodule Deparam do
+  @moduledoc """
+  Documentation for `Deparam`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Deparam.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/deparam_test.exs
@@ -0,0 +1,8 @@
+defmodule DeparamTest do
+  use ExUnit.Case
+  doctest Deparam
+
+  test "greets the world" do
+    assert Deparam.hello() == :world
+  end
+end
